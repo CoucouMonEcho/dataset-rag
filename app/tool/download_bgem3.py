c1814b38fb754231d110b00f137791b2c5054402
coucoumonecho <coucoumonecho@gmail.com>
2026-04-19 22:08:26 +0800

    graph node_item_name_recognition

--- app/tool/download_bgem3.py
+++ app/tool/download_bgem3.py
@@ -1,5 +1,6 @@
 from modelscope.hub.snapshot_download import snapshot_download
 
 # 下载模型到当前目录下的 models/bge-m3 文件夹
-model_dir = snapshot_download('BAAI/bge-m3', cache_dir='D:/ai_models/modelscope_cache/models')
+# uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+model_dir = snapshot_download('BAAI/bge-m3', cache_dir='D:\Workspace/models')
 print(f"模型已下载到: {model_dir}")
--- app/tool/download_reranker.py
+++ app/tool/download_reranker.py
@@ -1,7 +1,7 @@
 from modelscope.hub.snapshot_download import snapshot_download
 
-local_dir = r"D:\ai_models\modelscope_cache\models\rerank"
-
+local_dir = r"D:\Workspace\models\rerank"
+# uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 snapshot_download(
     model_id="BAAI/bge-reranker-large",
     cache_dir=local_dir,
